2feddac25402ef46a0cd8a020ab9a4ebb4e6f50d
Skeletus <83602040+Skeletus@users.noreply.github.com>
2024-06-04 11:42:47 -0500

    added gun script - function to modify size of box collider trigger

---
+++ 3DTopDownTankGame/Assets/Scripts/Gun.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] private float range = 20f;
+    [SerializeField] private float verticalRange = 20f;
+
+    private BoxCollider boxColliderTrigger;
+
+    private void Awake()
+    {
+        boxColliderTrigger = GetComponent<BoxCollider>();
+    }
+
+    private void Start()
+    {
+        boxColliderTrigger.size = new Vector3(1, verticalRange, range);
+        boxColliderTrigger.center = new Vector3(0, 0, range * 0.5f);
+    }
+}
