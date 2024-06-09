888d4840430a8648d68917ecf8ff0d0e4a7b02ba
Skeletus <83602040+Skeletus@users.noreply.github.com>
2024-06-09 13:23:58 -0500

    updated enemy awarness script

--- 3DTopDownTankGame/Assets/Scripts/EnemyAwarness.cs
+++ 3DTopDownTankGame/Assets/Scripts/EnemyAwarness.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class EnemyAwarness : MonoBehaviour
 {
     [SerializeField] private Material aggresiveMaterial;
+    private bool isAggro;
+
+    private void Update()
+    {
+        if (isAggro)
+        {
+            GetComponent<Renderer>().material = aggresiveMaterial;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.CompareTag("Player"))
         {
-            GetComponent<MeshRenderer>().material = aggresiveMaterial;
+            isAggro = true;
         }
     }
 }
